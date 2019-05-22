7e94a25efa792156853c21cb2cd6138dae6acba2
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-05-22 23:22:03 +0900

    リフレクションを使ったプライベートメソッド呼び出し

--- src/main/scala/ReflectionChallenge.scala
+++ src/main/scala/ReflectionChallenge.scala
@@ -1,12 +1,13 @@
 import scala.reflect.runtime.universe._
 
 class Issue(private val title: String) {
-    private def printTitle(): Unit = println(title)
+  private def printTitle(): Unit = println(title)
 }
 
 object ReflectionChallenge extends App {
-    val issue = new Issue("不具合1")
-
-      // TODO リフレクションを用いて printTitle() メソッドを呼び出す
+  val issue = new Issue("不具合1")
+  val mirror = runtimeMirror(issue.getClass.getClassLoader)
+  val instance = mirror.reflect(issue)
+  val printTitleMethod = typeTag[Issue].tpe.decl(TermName("printTitle")).asMethod // Methodを呼び出す
+  instance.reflectMethod(printTitleMethod).apply() // invokerミラー
 }
-
